d542f039415f03286d1b56ab6944be92dc90b83e
jero77 <34156935+jero77@users.noreply.github.com>
2018-07-10 19:15:35 +0200

    bundesliga exercise, 1st try

---
+++ ASP/bundesliga.s
@@ -0,0 +1,61 @@
+round(29..34).
+p(29..68). %% maximally 68P can be reached by the current leader
+
+% teams
+team(d). team(n). team(ki). team(r). team(in). team(bo).
+team(bi). team(au). team(svs). team(du). team(un). team(bs).
+team(dd). team(sp). team(ft). team(hdh). team(da). team(kl).
+
+% standings after 29 rounds:
+points(29,d,53).
+points(29,n,50).
+points(29,ki,46).
+points(29,r,41).
+points(29,in,41).
+points(29,bo,40).
+points(29,bi,40).
+points(29,au,39).
+points(29,svs,38).
+points(29,du,38).
+points(29,un,37).
+points(29,bs,37).
+points(29,dd,37).
+points(29,sp,37).
+points(29,ft,37).
+points(29,hdh,34).
+points(29,da,32).
+points(29,kl,29).
+
+% games of the remaining rounds:
+game(30,in,n). game(30,hdh,d). game(30,dd,ki).
+game(30,du,svs). game(30,ft,r). game(30,sp,un).
+game(30,bi,au). game(30,da,bs). game(30,bo,kl).
+
+game(31,ki,n). game(31,d,in). game(31,au,du).
+game(31,r,sp). game(31,svs,da). game(31,un,hdh).
+game(31,bs,bi). game(31,kl,dd). game(31,ft,bo).
+
+game(32,n,bs). game(32,dd,d). game(32,in,ki).
+game(32,du,r). game(32,hdh,svs). game(32,da,un).
+game(32,bi,kl). game(32,sp,ft). game(32,bo,au).
+
+game(33,svs,n). game(33,d,ki). game(33,ft,du).
+game(33,r,da). game(33,un,bo). game(33,sp,bi).
+game(33,bs,in). game(33,kl,hdh). game(33,au,dd).
+
+game(34,n,d). game(34,ki,bs). game(34,du,sp).
+game(34,bo,r). game(34,dd,un). game(34,bi,svs).
+game(34,in,kl). game(34,hdh,ft). game(34,da,au).
+
+
+% game can be won/lost/drawn, consider home team & deduct for other team
+% D = Day, H = Home, O = other
+1{win(D, H), loose(D, H), draw(D, H)}1 :- game(D, H, _).
+loose(D, O) :- win(D, H), game(D, H, O).
+win(D, O) :- loose(D, H), game(D, H, O).
+draw(D, O) :- draw(D, H), game(D, H, O).
+
+% win -> +3pts, loose -> +0 pts, draw -> +1pts
+points(R, T, P) :- points(R-1, T, X), win(R, T), P is X + 3.
+points(R, T, P) :- points(R-1, T, X), loose(R, T), P is X.
+points(R, T, P) :- points(R-1, T, X), draw(R, T), P is X + 1.
